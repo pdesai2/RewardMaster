1d905f87332e12d1e7e5e3ff0c997e4a7969a0c8
pdesai2 <Pratik.Desai@intuit.com>
2015-03-18 11:40:22 -0700

    Initial Commit

---
+++ RewardMaster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RewardMaster
+//
+//  Created by Desai, Pratik on 3/18/15.
+//  Copyright (c) 2015 intuit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
